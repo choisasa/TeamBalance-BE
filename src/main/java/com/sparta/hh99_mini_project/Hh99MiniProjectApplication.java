737fe63ebfedb31771589cbeb0a7b993e823d56a
yuha <yhj230508@gmail.com>
2024-03-12 14:44:09 +0900

    first commit

---
+++ src/main/java/com/sparta/hh99_mini_project/Hh99MiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hh99_mini_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hh99MiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hh99MiniProjectApplication.class, args);
+    }
+
+}
